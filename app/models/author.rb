56d6a3f023474d8153ea407c6382c74bf500b0aa
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2019-02-27 05:04:02 +0000

    Automatically backed up by Learn

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,8 +1,8 @@
 class Author < ActiveRecord::Base
-  #all authors have a name 
-  validates :name, presence: true 
-  #No two authors have the same name 
-  validates_uniqueness :name 
-  #Author phone number are 10 digits 
-  
+  #all authors have a name
+  validates :name, presence: true
+  #No two authors have the same name
+  validates_uniqueness :name
+  #Author phone number are 10 digits
+
 end
