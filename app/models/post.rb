938b3ad16043d2799da056111c3dcad2d07f4bfa
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2019-02-27 05:03:03 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,10 @@
 class Post < ActiveRecord::Base
+  #All posts have a title 
+  validates :title 
+  #Post content is at least 250 characters 
+
+  #post summary is maximum of 250 characters 
+
+  #Post category is either fiction or non-fiction 
+  inclusion 
 end
