af5df47287780a1782395d4bf32b42434f705866
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2019-02-27 05:34:37 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,9 +7,9 @@ class Post < ActiveRecord::Base
   validates :summary, length: { maximum: 250}
   #Post category is either fiction or non-fiction
   validates :category, inclusion: { in: %w(Fiction Non-fiction)}
-  validate :clickbait? 
+  validate :clickbait?
 
-  def clickbait? 
+  def clickbait?
     if self.title && !self.title.match(/Won't Believe|Secret|Top [\d*]|Guess/)
     errors.add(:clickbait, "is not enticing enough")
   end
