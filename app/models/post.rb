d5843cde0f5041230a7c9507369c3c9070204eaf
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2019-02-27 05:07:06 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,10 @@
 class Post < ActiveRecord::Base
   #All posts have a title
-  validates :title
+  validates :title, presence: true 
   #Post content is at least 250 characters
-
+  validates :content, length: { minimum: 250}
   #post summary is maximum of 250 characters
-
+  validates :summary, length: { maximum: 250}
   #Post category is either fiction or non-fiction
   inclusion
 end
